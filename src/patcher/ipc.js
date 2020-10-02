0e14b8c287ee79420520e305687ccfa18a9fbba9
intrnl <intrnl28+git@gmail.com>
2020-10-02 08:27:16 +0700

    refactor(patcher): use shared alias

--- src/patcher/ipc.js
+++ src/patcher/ipc.js
@@ -1,6 +1,6 @@
 import { ipcMain } from 'electron';
 
-import { IPC_GET_PRELOAD } from '../shared/constants/ipc.js';
+import { IPC_GET_PRELOAD } from '@shared/constants/ipc.js';
 
 
 ipcMain.on(IPC_GET_PRELOAD, (ev) => ev.returnValue = ev.sender.__originalPreload);
