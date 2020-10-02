8d448e56602ad480f3ec9d3c8521e67dbe980df0
intrnl <intrnl28+git@gmail.com>
2020-10-02 08:13:10 +0700

    docs(shared): add comments regarding how it works

--- src/shared/utils/root.js
+++ src/shared/utils/root.js
@@ -10,6 +10,13 @@ export let isContextIsolated = false;
 try {
 	let { webFrame } = require('electron');
 
+	// Preload, when context isolation is enabled, runs on its own web frame
+	// and the parent attached to it is the actual website frame, which allows us
+	// to pretty much access its context without having to disable context
+	// isolation.
+
+	// The `top` field recurses when you are the absolute parent web frame.
+
 	root = webFrame.top.context;
 	rootType = 'preload';
 
