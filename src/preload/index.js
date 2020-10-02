eaf699e869583ebbc050aecf3a766575c81b013a
intrnl <intrnl28+git@gmail.com>
2020-10-02 07:52:36 +0700

    refactor(preload): comment out contextbridge patch

--- src/preload/index.js
+++ src/preload/index.js
@@ -1,4 +1,4 @@
-import { ipcRenderer, contextBridge } from 'electron';
+import { ipcRenderer } from 'electron';
 
 import * as pkg from '../../package.json';
 import { IPC_GET_PRELOAD } from '../shared/constants/ipc.js';
@@ -8,7 +8,7 @@ console.log('[Dissonance]', `Running version ${pkg.version}`);
 
 // Discord uses contextBridge to expose DiscordNative, but the method throws
 // if context isolation is disabled, so we have to void it.
-contextBridge.exposeInMainWorld = () => void 0;
+// contextBridge.exposeInMainWorld = () => void 0;
 
 // Load any original preload if present.
 let originalPreload = ipcRenderer.sendSync(IPC_GET_PRELOAD);
